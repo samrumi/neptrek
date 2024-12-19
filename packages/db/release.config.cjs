5dd86ee06fee1d949abf890bd3225ecc30a9fb9d
sameerth.dev <sameerth.dev@gmail.com>
2024-12-19 19:46:01 +0545

    fix(release.yml): 🎨 Changed release flow

--- packages/db/release.config.cjs
+++ packages/db/release.config.cjs
@@ -1,3 +1,12 @@
 module.exports = {
   branches: ["main", { name: "next", prerelease: true }],
+  git: {
+    notes: false,
+  },
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
 };
